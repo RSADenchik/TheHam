9c7668a08c8c03ba979e5e913b0b2ab426057b5e
RSADenchik <WorldRSA@gmail.com>
2016-10-24 19:38:47 +0300

    First-commit

---
+++ js/partials/main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    /* Button-search */
+    $('#search').on('click', function () {
+        $('#forma').toggle();
+    });
+    /* Slider-slick */
+    $(function() {
+        $(".rslides").responsiveSlides({
+          auto: false,
+            speed: 1000,
+            prevText: "<button class='Previous'></button>",
+            nextText: "<button class='Next'></button>",
+            timeout: 4000,
+            maxwidth: "110px",
+            nav: true,
+            navContainer: ".rslides"
+        });
+    });
+    // Go to the previous item
+    $("#myBtn").click(function(){
+        $("#myCarousel").carousel("prev");
+    });
+
+    // Go to the next item
+    $("#myBtn2").click(function(){
+        $("#myCarousel").carousel("next");
+    });
+    // Google maps
+    $(function() {
+        $("#map").googleMap({
+            scrollwheel: true
+        });
+
+        // Marker 1
+        $("#map").addMarker({
+            coords: [50.512706, 30.602495],
+            url: 'https://RSADenchik.github.io/second_psd'
+        });
+    })
+});

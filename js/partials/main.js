ddabcddb8a945c6c9b54c2d20c36d5430ff4c4b4
RSADenchik <WorldRSA@gmail.com>
2016-11-18 06:44:45 +0200

    #Fix by Denchik

--- js/partials/main.js
+++ js/partials/main.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
+
     /* Button-search desktop and tablet */
-    $('#search-ham').on('click', function () {
+
+     $('#search-ham').on('click', function () {
 
         var search_button = $('.search-button');
         var search_input = $('.search-input');
         var search_button_input = $('.search-button-input');
 
-        if ( search_button.hasClass('active-button') && search_button_input.hasClass('active-button-input') && search_input.hasClass('active-input')  ) {
+        if ( search_button.hasClass('active-button') && search_button_input.hasClass('active-button-input') && search_input.hasClass('active-input')) {
 
             search_button.removeClass('active-button');
             search_input.removeClass('active-input');
@@ -15,18 +17,22 @@ $(document).ready(function() {
             return true;
         }
         else {
+
             search_button.addClass('active-button');
             search_input.addClass('active-input');
             search_button_input.addClass('active-button-input');
         }
     });
+
     /* Button-search mobile divice */
+
     $('#m-search').on('click', function () {
+
         var m_search_btn = $('.m-btn-search');
         var m_search_input = $('.search-m-input');
         var m_search_input_btn = $('.m-btn');
 
-        if ( m_search_btn.hasClass('m-btn-active') && m_search_input.hasClass('active-m-input') && m_search_input_btn.hasClass('m-btn-input-active') ) {
+        if ( m_search_btn.hasClass('m-btn-active') && m_search_input.hasClass('active-m-input') && m_search_input_btn.hasClass('m-btn-input-active')) {
 
             m_search_btn.removeClass('m-btn-active');
             m_search_input.removeClass('active-m-input');
@@ -35,13 +41,17 @@ $(document).ready(function() {
             return true;
         }
         else {
+
             m_search_btn.addClass('m-btn-active');
             m_search_input.addClass('active-m-input');
             m_search_input_btn.addClass('m-btn-input-active');
+
         }
     });
+
     // Фильтр блока .work
-    var filterizd =  $('.filtr-container').filterizr( {
+
+    $('.filtr-container').filterizr( {
         animationDuration: 0.6,
         layout: 'sameSize',
         "delay": 25,
@@ -57,18 +67,22 @@ $(document).ready(function() {
             transition: 'all 0.5s ease-out 0ms'
         }
     });
+
     //Simple filter controls
+
     $('.simplefilter .work-li').click(function() {
         $('.simplefilter .work-li').removeClass('active');
         $(this).addClass('active');
     });
 
     // Selected first li
+
     $('.work-menu .work-li').hover(function() {
         $('.work-menu li').removeClass('selected');
     });
 
-    /* Slider-slick */
+    /* Slider-slick home */
+
     $(function() {
         $(".rslides").responsiveSlides({
           auto: false,
@@ -81,32 +95,40 @@ $(document).ready(function() {
             navContainer: ".rslides"
         });
     });
-    // Go to the previous item
+
+    // Go to the previous item carousel
+
     $("#myBtn").click(function(){
         $("#myCarousel").carousel("prev");
     });
 
-    // Go to the next item
+    // Go to the next item carousel
+
     $("#myBtn2").click(function(){
         $("#myCarousel").carousel("next");
     });
+
     // Meet our team input add image
     $('.input-1').on('change', function() {
+
         // Берем название загруженое в input и выводим в блок
         var filename1 = $(this).val().replace(/.*\\/, " ");
         $('#filename-1').val(filename1).css("display", "block");
         $('#input-file-1').css("display", "block");
     });
+
     $('.input-2').on('change', function() {
         var filename2 = $(this).val().replace(/.*\\/, " ");
         $('#filename-2').val(filename2).css("display", "block");
         $('#input-file-2').css("display", "block");
     });
+
     $('.input-3').on('change', function() {
         var filename3 = $(this).val().replace(/.*\\/, " ");
         $('#filename-3').val(filename3).css("display", "block");
         $('#input-file-3').css("display", "block");
     });
+
     $('.input-4').on('change', function() {
         var filename4 = $(this).val().replace(/.*\\/, " ");
         $('#filename-4').val(filename4).css("display", "block");
@@ -129,9 +151,10 @@ $(document).ready(function() {
         $(this).hide();
         $('#filename-4').hide();
     });
+
     //upload images
-    $(document).ready (function (e) {
-        $(".hover-add-img").on('submit',(function(e) {
+
+    $(".hover-add-img").on('submit',(function(e) {
             e.preventDefault();
             $.ajax({
                 url: "ajaxupload.php",
@@ -164,35 +187,27 @@ $(document).ready(function() {
                 }
             });
         }));
-    });
-    // Google maps
-    $(function() {
-        $("#map").googleMap({
-            scrollwheel: true
-        });
 
-        // Marker 1
-        $("#map").addMarker({
+    // Google maps
+
+    $(function() {
+        $("#map").googleMap({scrollwheel: true}).addMarker({
             coords: [50.512706, 30.602495],
             url: 'https://RSADenchik.github.io/TheHam'
         });
     });
     // Google maps for mobile
     $(function() {
-        $("#map-m").googleMap({
-            scrollwheel: true
-        });
-
-        // Marker 1
-        $("#map-m").addMarker({
+        $("#map-m").googleMap({scrollwheel: true}).addMarker({
             coords: [50.512706, 30.602495],
             url: 'https://RSADenchik.github.io/TheHam'
         });
     });
         // Форма отправки сообщения
+
     $("#commentForm").submit(function() { //устанавливаем событие отправки для формы с id=form
         var form_data = $(this).serialize(); //собераем все данные из формы
-            $.ajax({
+        $.ajax({
                 type: "POST", //Метод отправки
                 url: "mail.php", //путь до php фаила отправителя
                 data: form_data,
@@ -204,8 +219,6 @@ $(document).ready(function() {
                         var inputName = $('.input-name').val();
                         alert(inputName + ", " + "письмо успешно отправлено!");
                 }
-            });
         });
-
-        // console.log(form_data);
+    });
 });

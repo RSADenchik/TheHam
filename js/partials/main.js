8103cd6fbba0075456ee62d2252c6bc00c8c5948
RSADenchik <WorldRSA@gmail.com>
2016-11-22 10:22:38 +0200

    #Last Fix

--- js/partials/main.js
+++ js/partials/main.js
@@ -1,19 +1,176 @@
 $(document).ready(function() {
 
-    /* Button-search desktop and tablet */
+    // Home beckground fade
+    $('.home').fadeOut(10).fadeIn(4000);
+    $('.content').fadeOut(10).fadeIn(3000);
 
+    // ease scroll on header link
+    $('.scrolling').click( function(){
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 4000, 'linear');
+        }
+        return false;
+    });
+
+    // Fade element without mobile
+    var md = new MobileDetect(window.navigator.userAgent);
+    if ( ! md.mobile() ) {
+        $('.slide-bottom').fadeThis({
+            speed: 900,
+            distance: -50,
+            reverse: false
+        });
+        $('.slide-left').fadeThis({
+            speed: 1500,
+            distance: 200,
+            reverse: false
+        });
+    }
+    // Header on scroll
+    $(document).on('scroll', function() {
+
+        var header = $('header');
+        var menuList = $('.menu-list');
+        var menuListLink = $('.menu-list > .list-items > a');
+
+        if($(document).scrollTop()>100) {
+            header.removeClass('header', 'linear').addClass('header-small', 'ease-in-out');
+            menuList.css('height', '74px');
+            menuListLink.css('padding-bottom', '33px');
+        }
+        else {
+            header.removeClass('header-small').addClass('header', 'ease-in-out');
+            menuList.css('height', '92px');
+            menuListLink.css('padding-bottom', '50px');
+        }
+    });
+    // End Header on scroll
+
+    // Event on scroll element
+
+    $(window).scroll(function() {
+        var y = $(document).scrollTop(); // Height about of top page
+        var h = $(window).height();  // Height of viewport
+        var about = $('.about'); // target about
+        var purchase = $('.purchase'); // target purchase
+        var purchase1 = $('.purchase-1');
+
+        // If scroll  up!
+        if(y + h < about.position().top) {
+            about.css({
+                backgroundPositionX: '-74%'
+            });
+        }
+        // If target partical in viewport
+        else if(y + h < about.position().top + about.outerHeight()) {
+            about.css({
+                backgroundPositionX: '4%',
+                transition: '5s ease'
+            });
+        }
+        // If scroll bottom!
+        else if(y > about.position().top + about.outerHeight()) {
+            about.css({
+                backgroundPositionX: '-74%',
+                transition: '1s ease'
+            });
+        }
+        // If target partical in viewport
+        else if(y > about.position().top) {
+            about.css({
+                backgroundPositionX: '4%',
+                transition: '5s ease'
+            });
+        }
+        // If scroll on target
+        else {
+            about.css({
+                backgroundPositionX: '4%'
+            });
+        }
+        // End about
+
+        // Start purchase1
+        if(y + h < purchase.position().top) {
+            purchase.css({
+                opacity: 0
+            });
+        }
+        // If target partical in viewport
+        else if(y + h < purchase.position().top + purchase.outerHeight()) {
+            purchase.css({
+                opacity: 1,
+                transition: '8s'
+            });
+        }
+        // If scroll bottom!
+        else if(y > purchase.position().top + purchase.outerHeight()) {
+            purchase.css({
+                opacity: 0,
+                transition: '2s'
+            });
+        }
+        // If target partical in viewport
+        else if(y > purchase.position().top) {
+            purchase.css({
+                opacity: 1,
+                transition: '8s'
+            });
+        }
+        // If scroll on target
+        else {
+            purchase.css({
+                opacity: 1
+            });
+        }
+        // If scroll  up!
+        if(y + h < purchase1.position().top) {
+            purchase1.css({
+                backgroundPositionX: '-74%'
+            });
+        }
+        // If target partical in viewport
+        else if(y + h < purchase1.position().top + purchase1.outerHeight()) {
+            purchase1.css({
+                backgroundPositionX: '4%',
+                transition: '5s ease'
+            });
+        }
+        // If scroll bottom!
+        else if(y > purchase1.position().top + purchase1.outerHeight()) {
+            purchase1.css({
+                backgroundPositionX: '-74%',
+                transition: '1s ease'
+            });
+        }
+        // If target partical in viewport
+        else if(y > purchase1.position().top) {
+            purchase1.css({
+                backgroundPositionX: '4%',
+                transition: '5s ease'
+            });
+        }
+        // If scroll on target
+        else {
+            purchase1.css({
+                backgroundPositionX: '4%'
+            });
+        }
+        // End purchase1
+    });
+
+    // Button-search desktop and tablet
      $('#search-ham').on('click', function () {
 
         var search_button = $('.search-button');
         var search_input = $('.search-input');
         var search_button_input = $('.search-button-input');
 
-        if ( search_button.hasClass('active-button') && search_button_input.hasClass('active-button-input') && search_input.hasClass('active-input')) {
-
+        if(search_button.hasClass('active-button') && search_button_input.hasClass('active-button-input') && search_input.hasClass('active-input')) {
             search_button.removeClass('active-button');
             search_input.removeClass('active-input');
             search_button_input.removeClass('active-button-input');
-
             return true;
         }
         else {
@@ -25,7 +182,6 @@ $(document).ready(function() {
     });
 
     /* Button-search mobile divice */
-
     $('#m-search').on('click', function () {
 
         var m_search_btn = $('.m-btn-search');
@@ -50,7 +206,6 @@ $(document).ready(function() {
     });
 
     // Фильтр блока .work
-
     $('.filtr-container').filterizr( {
         animationDuration: 0.6,
         layout: 'sameSize',
@@ -69,20 +224,17 @@ $(document).ready(function() {
     });
 
     //Simple filter controls
-
     $('.simplefilter .work-li').click(function() {
         $('.simplefilter .work-li').removeClass('active');
         $(this).addClass('active');
     });
 
     // Selected first li
-
     $('.work-menu .work-li').hover(function() {
         $('.work-menu li').removeClass('selected');
     });
 
     /* Slider-slick home */
-
     $(function() {
         $(".rslides").responsiveSlides({
           auto: false,
@@ -97,17 +249,14 @@ $(document).ready(function() {
     });
 
     // Go to the previous item carousel
-
     $("#myBtn").click(function(){
         $("#myCarousel").carousel("prev");
     });
 
     // Go to the next item carousel
-
     $("#myBtn2").click(function(){
         $("#myCarousel").carousel("next");
     });
-
     // Meet our team input add image
     $('.input-1').on('change', function() {
 
@@ -187,6 +336,13 @@ $(document).ready(function() {
                 }
             });
         }));
+    // Serealize data fron checkbox
+    $('.purchase-1-button').on('click', function() {
+        var formCheck = $('.ul-purchase-1 input:checkbox:checked');
+        formCheck.each(function(){
+            alert($(this).val());
+        });
+    });
 
     // Google maps
 
